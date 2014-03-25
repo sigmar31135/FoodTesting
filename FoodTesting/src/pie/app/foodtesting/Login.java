97b674ce8a6624f280f84a8822be6c841105ef9b
Sigmar <sigmar31135@gmail.com>
2014-03-25 19:56:49 +0700

    config log in page that pass value in name user_id

--- FoodTesting/src/pie/app/foodtesting/Login.java
+++ FoodTesting/src/pie/app/foodtesting/Login.java
@@ -1,16 +1,23 @@
 package pie.app.foodtesting;
 
+import database.MainDatabase;
 import database.ProductTable;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends Activity{
 
-	Button SignIn,SignUp;
+	private static Button SignIn,SignUp;
+	private static EditText edtUsername,edtPassword;
+	private static String username,password;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,8 @@ public class Login extends Activity{
 		// TODO Auto-generated method stub
 		SignIn = (Button)findViewById(R.id.btn_signin);
 		SignUp = (Button)findViewById(R.id.btn_signup);
+		edtUsername = (EditText) findViewById(R.id.login_username);
+		edtPassword = (EditText) findViewById(R.id.login_password);
 	}
 	
 	OnClickListener listener = new OnClickListener() {
@@ -44,13 +53,36 @@ public class Login extends Activity{
 			// TODO Auto-generated method stub
 			switch (v.getId()) {
 			case R.id.btn_signin:
-						Intent intent = new Intent(Login.this,MainActivity.class);
-						startActivity(intent);
+						username = edtUsername.getText().toString().trim();
+						password  = edtPassword.getText().toString().trim();
+						if(!(username.isEmpty()||password.isEmpty())){
+							String passValue = CheckUser();
+							if(passValue.equals("error"))
+								Toast.makeText(Login.this, "Invalid username or password.", Toast.LENGTH_SHORT).show();
+							else{
+								Intent intent = new Intent(Login.this,MainActivity.class);
+								intent.putExtra(MainDatabase.UserTableColUserId, passValue);
+								startActivity(intent);
+							}
+						}else{
+							Toast.makeText(getApplicationContext(),"Please insert username or password.",Toast.LENGTH_SHORT).show();
+						}
 				break;
 
 			default:
 				break;
 			}
+		}
+
+		private String CheckUser() {
+			// TODO Auto-generated method stub
+			SQLiteDatabase objSQL = new MainDatabase(Login.this).getReadableDatabase();
+			Cursor Data = objSQL.query(MainDatabase.UserTableName, new String[]{MainDatabase.UserTableColUserId}, MainDatabase.UserTableColUser + " = '" + username + "' AND " + MainDatabase.UserTableColPassword + " ='"+ password + "'", null, null, null, null);
+			Data.moveToFirst();
+			if(Data.getCount()!=0){
+				return Data.getString(Data.getColumnIndex(MainDatabase.UserTableColUserId));
+			}
+			return "error";
 		}
 	};
 }
