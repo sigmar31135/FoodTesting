d37e5239dd750796c4f7f0fd53a331b354b67a4b
Bulakorn <keng_bd2@hotmail.com>
2014-03-26 21:40:22 +0700

    button color

--- FoodTesting/gen/pie/app/foodtesting/R.java
+++ FoodTesting/gen/pie/app/foodtesting/R.java
@@ -10,6 +10,10 @@ package pie.app.foodtesting;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int green=0x7f070000;
+        public static final int green_press=0x7f070001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -21,36 +25,37 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int images=0x7f020001;
-        public static final int logo2=0x7f020002;
+        public static final int button_action=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int images=0x7f020002;
+        public static final int logo2=0x7f020003;
     }
     public static final class id {
-        public static final int MainActivity=0x7f080015;
-        public static final int ResultActivity=0x7f080018;
-        public static final int WaitingActivity=0x7f080016;
-        public static final int WorkActivity=0x7f080017;
-        public static final int attribute_done=0x7f080002;
-        public static final int attribute_name=0x7f080008;
-        public static final int btn_signin=0x7f080010;
-        public static final int btn_signup=0x7f080011;
-        public static final int btn_test=0x7f080004;
-        public static final int display_point=0x7f080009;
-        public static final int expandableListView=0x7f080001;
-        public static final int gridView=0x7f080000;
-        public static final int imageView1=0x7f08000d;
-        public static final int login_password=0x7f08000f;
-        public static final int login_username=0x7f08000e;
-        public static final int parent_scale=0x7f08000b;
-        public static final int result_attribute_name=0x7f080005;
-        public static final int result_attribute_point=0x7f080006;
-        public static final int result_done=0x7f080013;
-        public static final int result_expendableList=0x7f080012;
-        public static final int seekBar=0x7f08000a;
-        public static final int test_id=0x7f080003;
-        public static final int textView1=0x7f08000c;
-        public static final int txt_attribute=0x7f080007;
-        public static final int txt_scale=0x7f080014;
+        public static final int MainActivity=0x7f090015;
+        public static final int ResultActivity=0x7f090018;
+        public static final int WaitingActivity=0x7f090016;
+        public static final int WorkActivity=0x7f090017;
+        public static final int attribute_done=0x7f090002;
+        public static final int attribute_name=0x7f090008;
+        public static final int btn_signin=0x7f090010;
+        public static final int btn_signup=0x7f090011;
+        public static final int btn_test=0x7f090004;
+        public static final int display_point=0x7f090009;
+        public static final int expandableListView=0x7f090001;
+        public static final int gridView=0x7f090000;
+        public static final int imageView1=0x7f09000d;
+        public static final int login_password=0x7f09000f;
+        public static final int login_username=0x7f09000e;
+        public static final int parent_scale=0x7f09000b;
+        public static final int result_attribute_name=0x7f090005;
+        public static final int result_attribute_point=0x7f090006;
+        public static final int result_done=0x7f090013;
+        public static final int result_expendableList=0x7f090012;
+        public static final int seekBar=0x7f09000a;
+        public static final int test_id=0x7f090003;
+        public static final int textView1=0x7f09000c;
+        public static final int txt_attribute=0x7f090007;
+        public static final int txt_scale=0x7f090014;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -67,7 +72,7 @@ public final class R {
         public static final int work_activity=0x7f03000b;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
