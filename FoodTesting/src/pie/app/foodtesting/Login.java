af90a994346e5408d0875f80be45e31b8c6c196b
Sigmar <sigmar31135@gmail.com>
2014-03-29 20:32:40 +0700

    change permission

--- FoodTesting/src/pie/app/foodtesting/Login.java
+++ FoodTesting/src/pie/app/foodtesting/Login.java
@@ -1,8 +1,13 @@
 package pie.app.foodtesting;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import org.apache.http.client.HttpClient;
@@ -13,18 +18,19 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends Activity{
+public class Login extends Activity {
 
 	private static Button SignIn;
-	private static EditText edtUsername,edtPassword;
-	private static String username,password;
-	
+	private static EditText edtUsername, edtPassword;
+	private static String username, password;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -32,10 +38,10 @@ public class Login extends Activity{
 		setContentView(R.layout.login);
 		initial();
 		set_button();
-		
+
 		edtUsername.setText("sigmar");
 		edtPassword.setText("gggggggg");
-		
+
 	}
 
 	private void set_button() {
@@ -45,76 +51,98 @@ public class Login extends Activity{
 
 	private void initial() {
 		// TODO Auto-generated method stub
-		SignIn = (Button)findViewById(R.id.btn_signin);
+		SignIn = (Button) findViewById(R.id.btn_signin);
 		edtUsername = (EditText) findViewById(R.id.login_username);
 		edtPassword = (EditText) findViewById(R.id.login_password);
 	}
-	
+
 	OnClickListener listener = new OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
 			// TODO Auto-generated method stub
 			switch (v.getId()) {
 			case R.id.btn_signin:
-						username = edtUsername.getText().toString().trim();
-						password  = edtPassword.getText().toString().trim();
-						if(!(username.isEmpty()||password.isEmpty())){
-							String passValue = "error";
-							if(passValue.equals("error"))
-								Toast.makeText(Login.this, "Invalid username or password.", Toast.LENGTH_SHORT).show();
-							else{
-								Intent intent = new Intent(Login.this,MainActivity.class);
-								intent.putExtra("user_id", passValue);
-								startActivity(intent);
-								
-								edtUsername.setText("");
-								edtPassword.setText("");
-							}
-						}else{
-							Toast.makeText(getApplicationContext(),"Please insert username or password.",Toast.LENGTH_SHORT).show();
-						}
+				username = edtUsername.getText().toString().trim();
+				password = edtPassword.getText().toString().trim();
+				if (!(username.isEmpty() || password.isEmpty())) {
+					String passValue = "error";
+					new CheckUser().execute();
+					if (passValue.equals("error"))
+						Toast.makeText(Login.this,
+								"Invalid username or password.",
+								Toast.LENGTH_SHORT).show();
+					else {
+						Intent intent = new Intent(Login.this,
+								MainActivity.class);
+						intent.putExtra("user_id", passValue);
+						startActivity(intent);
+
+						edtUsername.setText("");
+						edtPassword.setText("");
+					}
+				} else {
+					Toast.makeText(getApplicationContext(),
+							"Please insert username or password.",
+							Toast.LENGTH_SHORT).show();
+				}
 				break;
-					
+
 			default:
 				break;
 			}
 		}
 	};
-	
-	private class CheckUser extends AsyncTask<  Void, Void, String>{
+
+	private class CheckUser extends AsyncTask<Void, Void, String> {
 
 		@Override
 		protected String doInBackground(Void... params) {
 			// TODO Auto-generated method stub
-			
+
 			try {
-				 HttpURLConnection connectWeb = null;
-				 String Url = "http://food.tartecake.com/checklogin.php";
-				 connectWeb.setRequestMethod("GET");
-				 connectWeb.addRequestProperty("username", username);
-				 connectWeb.addRequestProperty("password", password);
-				 
-				 int resultServer = connectWeb.getResponseCode();
-				 if(resultServer == HttpURLConnection.HTTP_OK){
-					 InputStream objInpuStream = connectWeb.getInputStream();
-					 int read = 0;
-					 while (( read = objInpuStream.read()) != -1){
-						 
-						 
-						 
-					 }
-				 }
-				 
-				 }catch
-			
-			
-			
-			
+				HttpURLConnection connectWeb = null;
+				String url = "http://food.tartecake.com/checklogin.php";
+
+				connectWeb = (HttpURLConnection)new URL(url).openConnection();
+				connectWeb.setRequestMethod("GET");
+				connectWeb.addRequestProperty("username", username);
+				connectWeb.addRequestProperty("password", password);
+
+				int resultServer;
+
+				resultServer = connectWeb.getResponseCode();
+
+				if (resultServer == HttpURLConnection.HTTP_OK) {
+
+					InputStream objInpuStream = connectWeb.getInputStream();
+					ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+					int read = 0;
+					while ((read = objInpuStream.read()) != -1) {
+						bos.write(read);
+					}
+					byte[] result = bos.toByteArray();
+					bos.close();
+
+					String data = new String(result);
+					Log.d("GGGGGG", data);
+					return data;
+				} else {
+					return "error";
+
+				}
+			} catch (ProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 			return null;
 		}
-		
+
 	}
-	
-	
+
 }
